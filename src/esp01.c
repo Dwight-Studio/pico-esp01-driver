ace38dd50f9a91c6d17f14e89c234e91d87f3092
Alexandre Leconte <aleconte@dwightstudio.fr>
2024-07-09 19:58:53 +0200

    Adding all useful AT commands

--- src/esp01.c
+++ src/esp01.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include "esp01.h"
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+
+esp01_inst_t esp01_init(uart_inst_t *uart_inst, uint baud_rate, uint tx_pin, uint rx_pin) {
+    esp01_inst_t inst;
+
+    gpio_set_function(tx_pin, GPIO_FUNC_UART);
+    gpio_set_function(rx_pin, GPIO_FUNC_UART);
+
+    inst.uart_inst = uart_inst;
+    inst.baud_rate = uart_init(uart_inst, baud_rate);
+
+    return inst;
 }
