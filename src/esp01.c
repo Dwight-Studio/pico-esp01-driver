09c081619eeabb7dbf705f8f2a1f405c9de403de
Alexandre Leconte <aleconte@dwightstudio.fr>
2024-07-30 00:43:44 +0200

    Adding basic Wifi commands

--- src/esp01.c
+++ src/esp01.c
@@ -2,11 +2,6 @@
 
 #define ESP01_DRIVER_DEBUG
 
-void esp01_setup_default_uart(uart_inst_t *uart_inst) {
-    uart_set_format(uart_inst, ESP01_DEFAULT_DATA_BITS, ESP01_DEFAULT_STOP_BITS, ESP01_DEFAULT_PARITY);
-    uart_set_hw_flow(uart_inst, ESP01_DEFAULT_CTS, ESP01_DEFAULT_RTS);
-    uart_set_translate_crlf(uart_inst, false);
-}
 
 esp01_inst_t *esp01_init(uart_inst_t *uart_inst, uint baud_rate, uint tx_pin, uint rx_pin) {
     esp01_inst_t *inst = malloc(sizeof(esp01_inst_t));
@@ -19,8 +14,8 @@ esp01_inst_t *esp01_init(uart_inst_t *uart_inst, uint baud_rate, uint tx_pin, ui
 
     // Setup UART communication with default configuration
     inst->uart_inst = uart_inst;
-    inst->baud_rate = uart_init(uart_inst, baud_rate);
-    esp01_setup_default_uart(uart_inst);
+    inst->uart_settings.baud_rate = baud_rate;
+    esp01_reinit(inst);
 
     return inst;
 }
@@ -35,23 +30,25 @@ void esp01_deinit(esp01_inst_t *inst) {
 }
 
 void esp01_reinit(esp01_inst_t *inst) {
-    inst->baud_rate = uart_init(inst->uart_inst, inst->baud_rate);
-    esp01_setup_default_uart(inst->uart_inst);
+    inst->uart_settings.baud_rate = uart_init(inst->uart_inst, inst->uart_settings.baud_rate);
+    uart_set_format(inst->uart_inst, ESP01_DEFAULT_DATA_BITS, ESP01_DEFAULT_STOP_BITS, ESP01_DEFAULT_PARITY);
+    uart_set_hw_flow(inst->uart_inst, ESP01_DEFAULT_CTS, ESP01_DEFAULT_RTS);
+    uart_set_translate_crlf(inst->uart_inst, false);
 }
 
-void esp01_set_host_uart(esp01_inst_t *inst, uint data_bits, uint stop_bits, uart_parity_t parity, bool cts, bool rts) {
-    uart_set_hw_flow(inst->uart_inst, cts, rts);
-    uart_set_format(inst->uart_inst, data_bits, stop_bits, parity);
+esp01_uart_settings_t esp01_get_host_uart(esp01_inst_t *inst) {
+    return inst->uart_settings;
 }
 
-void esp01_set_host_uart_from_device(esp01_inst_t *inst, esp01_uart_settings_t uart_set) {
-    if (inst->baud_rate != uart_set.baud_rate) uart_set_baudrate(inst->uart_inst, uart_set.baud_rate);
-    esp01_set_host_uart(inst, uart_set.data_bits, uart_set.stop_bits, uart_set.parity, uart_set.cts, uart_set.rts);
+void esp01_set_host_uart(esp01_inst_t *inst, esp01_uart_settings_t uart_set) {
+    if (inst->uart_settings.baud_rate != uart_set.baud_rate) uart_set_baudrate(inst->uart_inst, uart_set.baud_rate);
+    uart_set_hw_flow(inst->uart_inst, uart_set.cts, uart_set.rts);
+    uart_set_format(inst->uart_inst, uart_set.data_bits, uart_set.stop_bits, uart_set.parity);
 }
 
 char *esp01_at_cmd(esp01_inst_t *inst, uint timeout_ms, char cmd_mode, char *label, ...) {
     // Allocate memory for the command
-    char *o_cmd = malloc(ESP01_CMD_LENGTH);
+    char *o_cmd = malloc(ESP01_CMD_LENGTH + 1);
     char *cmd = o_cmd;
     *cmd = '\0';
 
@@ -104,7 +101,7 @@ char *esp01_at_cmd(esp01_inst_t *inst, uint timeout_ms, char cmd_mode, char *lab
     // Send the command if possible
     if (uart_is_writable(inst->uart_inst)) {
         // Allocate memory for the device response
-        char *o_rsp = malloc(ESP01_RSP_LENGTH);
+        char *o_rsp = malloc(ESP01_RSP_LENGTH + 1);
         char *rsp = o_rsp;
         *rsp = '\0';
 
@@ -193,6 +190,8 @@ bool esp01_rsp_ok_free(char *rsp) {
     return false;
 }
 
+// Basic
+
 bool esp01_test(esp01_inst_t *inst) {
     char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_EXECUTE, AT_TEST, "\n");
     return esp01_rsp_ok_free(rsp);
@@ -208,9 +207,9 @@ bool esp01_get_version(esp01_inst_t *inst, esp01_version_t *ver) {
     char *rsp = o_rsp;
 
     if (esp01_rsp_ok(rsp)) {
-        ver->at = malloc(ESP01_CMD_LENGTH);
-        ver->sdk = malloc(ESP01_CMD_LENGTH);
-        ver->bin = malloc(ESP01_CMD_LENGTH);
+        ver->at = malloc(ESP01_CMD_LENGTH + 1);
+        ver->sdk = malloc(ESP01_CMD_LENGTH + 1);
+        ver->bin = malloc(ESP01_CMD_LENGTH + 1);
 
         *(ver->at) = *(ver->sdk) = *(ver->bin) = '\0';
 
@@ -238,29 +237,13 @@ bool esp01_get_sleep_mode(esp01_inst_t *inst, esp01_sleep_mode_t *mode) {
     char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_QUERY, AT_SLEEP_CFG, "\n");
 
     if (esp01_rsp_ok(rsp)) {
-        int n = -1;
+        int m;
 
-        sscanf(rsp, "+SLEEP:%d", &n);
+        sscanf(rsp, "+SLEEP:%d", &m);
+
+        *mode = m;
 
         free(rsp);
-
-        switch (n) {
-            case 0:
-                *mode = DISABLED;
-                break;
-            case 1:
-                *mode = MODEM_DTIM;
-                break;
-            case 2:
-                *mode = LIGHT;
-                break;
-            case 3:
-                *mode = MODEM_INTERVAL;
-                break;
-            default:
-                return false;
-        }
-
         return true;
     } else {
         free(rsp);
@@ -269,31 +252,16 @@ bool esp01_get_sleep_mode(esp01_inst_t *inst, esp01_sleep_mode_t *mode) {
 }
 
 bool esp01_set_sleep_mode(esp01_inst_t *inst, esp01_sleep_mode_t mode) {
-    char *n;
+    char n[2];
 
-    switch (mode) {
-        case DISABLED:
-            n = "0";
-            break;
-        case MODEM_DTIM:
-            n = "1";
-            break;
-        case LIGHT:
-            n = "2";
-            break;
-        case MODEM_INTERVAL:
-            n = "3";
-            break;
-        default:
-            return false;
-    }
+    sprintf(n, "%d", mode);
 
     char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_SET, AT_SLEEP_CFG, n, "\n");
     return esp01_rsp_ok_free(rsp);
 }
 
 bool esp01_deep_sleep(esp01_inst_t *inst, uint duration) {
-    char cmd[ESP01_CMD_LENGTH];
+    char cmd[ESP01_CMD_LENGTH + 1];
     sprintf(cmd, "%u", duration);
 
     char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_SET, AT_DEEP_SLEEP, cmd, "\n");
@@ -312,7 +280,8 @@ bool esp01_get_uart_settings(esp01_inst_t *inst, esp01_uart_settings_t *uart_set
         uint parity;
         uint flow_control;
 
-        sscanf(rsp, current ? "+UART_CUR:%u,%u,%u,%u,%u" : "+UART_DEF:%u,%u,%u,%u,%u", &(uart_set->baud_rate), &(uart_set->data_bits), &(uart_set->stop_bits),
+        sscanf(rsp, current ? "+UART_CUR:%u,%u,%u,%u,%u" : "+UART_DEF:%u,%u,%u,%u,%u", &(uart_set->baud_rate),
+               &(uart_set->data_bits), &(uart_set->stop_bits),
                &parity, &flow_control);
 
         free(rsp);
@@ -384,10 +353,11 @@ bool esp01_set_uart_settings(esp01_inst_t *inst, esp01_uart_settings_t uart_set,
         flow_control += 2;
     }
 
-    char cmd[ESP01_CMD_LENGTH];
+    char cmd[ESP01_CMD_LENGTH + 1];
     sprintf(cmd, "%u,%u,%u,%u,%u", uart_set.baud_rate, uart_set.data_bits, uart_set.stop_bits, parity, flow_control);
 
-    char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_SET, current ? AT_UART_CURRENT : AT_UART_DEFAULT, cmd, "\n");
+    char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_SET, current ? AT_UART_CURRENT : AT_UART_DEFAULT, cmd,
+                             "\n");
     return esp01_rsp_ok_free(rsp);
 }
 
@@ -413,3 +383,137 @@ bool esp01_set_store_mode(esp01_inst_t *inst, bool mode) {
     char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_SET, AT_STORE_MODE, cmd, "\n");
     return esp01_rsp_ok_free(rsp);
 }
+
+// Wifi
+
+bool esp01_get_wifi_mode(esp01_inst_t *inst, esp01_wifi_mode_t *mode) {
+    char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_QUERY, AT_WIFI_MODE, "\n");
+
+    if (esp01_rsp_ok(rsp)) {
+        int m;
+
+        sscanf(rsp, "+CWMODE:%d", &m);
+
+        *mode = m;
+
+        free(rsp);
+        return true;
+    } else {
+        free(rsp);
+        return false;
+    }
+}
+
+bool esp01_set_wifi_mode(esp01_inst_t *inst, esp01_wifi_mode_t mode) {
+    char n[2];
+
+    sprintf(n, "%d", mode);
+
+    char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_SET, AT_WIFI_MODE, n, "\n");
+    return esp01_rsp_ok_free(rsp);
+}
+
+bool esp01_get_wifi_state(esp01_inst_t *inst, esp01_wifi_properties_t *state) {
+    char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_QUERY, AT_WIFI_STATE, "\n");
+
+    if (esp01_rsp_ok(rsp)) {
+        int s;
+        *(state->ssid) = '\0';
+
+        sscanf(rsp, "+CWSTATE:%d,\"%[^\"]\"", &s, state->ssid);
+
+        state->state = s;
+
+        free(rsp);
+        return true;
+    } else {
+        free(rsp);
+        return false;
+    }
+}
+
+bool esp01_get_wifi_connection(esp01_inst_t *inst, esp01_connection_properties_t *properties) {
+    char *rsp = esp01_at_cmd(inst, ESP01_DEFAULT_TIMEOUT, AT_QUERY, AT_WIFI_STATION_CONNECT, "\n");
+
+    if (esp01_rsp_ok(rsp)) {
+
+        int rtn = sscanf(rsp, "+CWJAP:\"%[^\"]\",\"%[^\"]\",%d,%d,%d,%d,%d,%d,%d", properties->ssid, properties->mac,
+               &(properties->channel), &(properties->rssi), &(properties->pci_auth),
+               &(properties->reconnection_interval), &(properties->listen_interval), &(properties->scan_mode),
+               &(properties->pmf));
+
+        free(rsp);
+        if (rtn == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        free(rsp);
+        return false;
+    }
+}
+
+bool esp01_wifi_connect(esp01_inst_t *inst, esp01_connection_properties_t properties, esp01_wifi_error_t *error_code) {
+    char cmd[ESP01_CMD_LENGTH + 1];
+
+    sprintf(cmd, "\"%s\",\"%s\"", properties.ssid, properties.password);
+
+    if (strlen(properties.mac) != 0) {
+        sprintf(cmd, "%s,%s", cmd, properties.mac);
+    } else {
+        sprintf(cmd, "%s,", cmd);
+    }
+
+    if (properties.pci_auth != ESP01_UNDEFINED) {
+        sprintf(cmd, "%s,%d", cmd, properties.pci_auth);
+    } else {
+        sprintf(cmd, "%s,", cmd);
+    }
+
+    if (properties.reconnection_interval != ESP01_UNDEFINED) {
+        sprintf(cmd, "%s,%d", cmd, properties.reconnection_interval);
+    } else {
+        sprintf(cmd, "%s,", cmd);
+    }
+
+    if (properties.listen_interval != ESP01_UNDEFINED) {
+        sprintf(cmd, "%s,%d", cmd, properties.listen_interval);
+    } else {
+        sprintf(cmd, "%s,", cmd);
+    }
+
+    if (properties.scan_mode != ESP01_UNDEFINED) {
+        sprintf(cmd, "%s,%d", cmd, properties.scan_mode);
+    } else {
+        sprintf(cmd, "%s,", cmd);
+    }
+
+    if (properties.jap_timeout != ESP01_UNDEFINED) {
+        sprintf(cmd, "%s,%d", cmd, properties.jap_timeout);
+    } else {
+        sprintf(cmd, "%s,", cmd);
+    }
+
+    if (properties.pmf != ESP01_UNDEFINED) {
+        sprintf(cmd, "%s,%d", cmd, properties.pmf);
+    } else {
+        sprintf(cmd, "%s,", cmd);
+    }
+
+    char *rsp = esp01_at_cmd(inst, ESP01_EXTRA_EXTENDED_TIMEOUT, AT_SET, AT_WIFI_STATION_CONNECT, cmd, "\n");
+
+    if (esp01_rsp_ok(rsp)) {
+        free(rsp);
+        return true;
+    } else {
+        int code;
+        sscanf(rsp, "+CWJAP:%d", &code);
+
+        *error_code = code;
+
+        free(rsp);
+        return false;
+    }
+}
+
--- src/esp01.h
+++ src/esp01.h
@@ -10,57 +10,75 @@
 #include "hardware/uart.h"
 #include "pico/malloc.h"
 
+#define ESP01_UNDEFINED (-1)
+
 #define ESP01_DEFAULT_BAUD_RATE 115273
 #define ESP01_DEFAULT_DATA_BITS 8
 #define ESP01_DEFAULT_STOP_BITS 1
 #define ESP01_DEFAULT_PARITY 0
 #define ESP01_DEFAULT_CTS false
 #define ESP01_DEFAULT_RTS true
+#define ESP01_SSID_LENGTH 32
+#define ESP01_MAC_LENGTH 17
+#define ESP01_PWD_LENGTH 64
 #define ESP01_CMD_LENGTH 256
 #define ESP01_RSP_LENGTH 4096
 #define ESP01_DEFAULT_TIMEOUT 1000
 #define ESP01_EXTENDED_TIMEOUT 10000
-#define ESP01_EXTRA_EXTENDED_TIMEOUT 10000
+#define ESP01_EXTRA_EXTENDED_TIMEOUT 20000
+
+#define ESP01_DEFAULT_CONNECTION_PROPERTIES {"", "", "", ESP01_UNDEFINED, ESP01_UNDEFINED, ESP01_UNDEFINED, ESP01_UNDEFINED, ESP01_UNDEFINED, ESP01_UNDEFINED, ESP01_UNDEFINED, ESP01_UNDEFINED}
 
 #define AT_QUERY '?'
 #define AT_SET '='
 #define AT_EXECUTE '\0'
 
-// Generic
-#define AT_TEST "AT"                    // [X] Test AT startup.
-#define AT_RESET "AT+RST"               // [X] Restart a module.
-#define AT_VERSION "AT+GMR"             // [X] Check version information.
-#define AT_LIST_COMMANDS "AT+CMD"       // [ ] List all AT commands and types supported in current firmware.
-#define AT_SLEEP_CFG "AT+SLEEP"         // [X] Set the sleep mode.
-#define AT_DEEP_SLEEP "AT+GSLP"         // [X] Enter Deep-sleep mode.
-#define AT_FACTORY_RESET "AT+RESTORE"   // [X] Restore factory default settings of the module.
-#define AT_UART_CURRENT "AT+UART_CUR"   // [X] Current UART Configuration, Not Saved in Flash.
-#define AT_UART_DEFAULT "AT+UART_DEF"   // [X] Default UART Configuration, Saved in Flash
-#define AT_STORE_MODE "AT+SYSSTORE"     // [X] Query/Set parameter store mode.
-#define AT_PROMPT_CFG "AT+SYSMSG"       // [ ] Query/Set System Prompt Information.
+// Basic
+#define AT_TEST "AT"                            // [X] Test AT startup.
+#define AT_ECHO_OFF "ATE0"                      // [ ] Disable command echoing.
+#define AT_ECHO_ON "ATE1"                       // [ ] Enable command echoing.
+#define AT_RESET "AT+RST"                       // [X] Restart a module.
+#define AT_VERSION "AT+GMR"                     // [X] Check version information.
+#define AT_LIST_COMMANDS "AT+CMD"               // [ ] List all AT commands and types supported in current firmware.
+#define AT_DEEP_SLEEP "AT+GSLP"                 // [X] Enter Deep-sleep mode.
+#define AT_LOCAL_TIMESTAMP "AT+SYSTIMESTAMP"    // [ ] Query/Set Local Time Stamp
+#define AT_SLEEP_CFG "AT+SLEEP"                 // [X] Set the sleep mode.
+#define AT_FACTORY_RESET "AT+RESTORE"           // [X] Restore factory default settings of the module.
+#define AT_RAM "AT+SYSRAM"                      // [ ] Query Current Remaining Heap Size and Minimum Heap Size.
+#define AT_FLASH "AT+SYSFLASH"                  // [ ] Query/Set User Partitions in Flash.
+#define AT_RF_POWER "AT+RFPOWER"                // [ ] Query/Set RF TX Power.
+#define AT_PROMPT "AT+SYSMSG"                   // [ ] Configure system prompt information.
+#define AT_FIRWARE_ROLLBACK "AT+SYSROLLBACK"    // [ ] Roll Back to the Previous Firmware.
+#define AT_STORE_MODE "AT+SYSSTORE"             // [X] Query/Set parameter store mode.
+#define AT_UART_CURRENT "AT+UART_CUR"           // [X] Current UART Configuration, Not Saved in Flash.
+#define AT_UART_DEFAULT "AT+UART_DEF"           // [X] Default UART Configuration, Saved in Flash.
 
 // Wifi
-#define AT_WIFI_MODE "AT+CWMODE"                    // [ ] Set the Wi-Fi mode (Station/SoftAP/Station+SoftAP).
-#define AT_WIFI_STATE "AT+CWSTATE"                  // [ ] Query the Wi-Fi state and Wi-Fi information.
-#define AT_WIFI_CONNECT_AP "AT+CWJAP"               // [ ] Connect to an AP.
-#define AT_WIFI_DISCONNECT_AP "AT+CWQAP"            // [ ] Disconnect from an AP.
+#define AT_WIFI_MODE "AT+CWMODE"                    // [X] Set the Wi-Fi mode (Station/SoftAP/Station+SoftAP).
+#define AT_WIFI_STATE "AT+CWSTATE"                  // [X] Query the Wi-Fi state and Wi-Fi information.
+#define AT_WIFI_STATION_CONNECT "AT+CWJAP"          // [X] Connect to an AP.
 #define AT_WIFI_RECONNECT_CFG "AT+CWRECONNCGF"      // [ ] Query/Set the Wi-Fi reconnecting configuration.
-#define AT_WIFI_LIST_APS_CFG "AT+CWLAPOPT"          // [ ] Set the configuration for the command AT+CWLAP.
-#define AT_WIFI_LIST_APS "AT+CWLAP"                 // [ ] List available APs.
-#define AT_WIFI_SOFT_AP_CFG "AT+CWSAP"              // [ ] Query/Set the configuration of an ESP SoftAP.
-#define AT_WIFI_LIST_STATIONS "AT+CWLIF"            // [ ] Obtain IP address of the station that connects to an ESP SoftAP.
-#define AT_WIFI_DISCONNECT_STATION "AT+CWQIF"       // [ ] Disconnect stations from an ESP SoftAP.
+#define AT_WIFI_LIST_NETWORKS "AT+CWLAP"            // [ ] List available APs.
+#define AT_WIFI_LIST_NETWORKS_CFG "AT+CWLAPOPT"     // [ ] Set the configuration for the command AT+CWLAP.
+#define AT_WIFI_STATION_DISCONNECT "AT+CWQAP"       // [ ] Disconnect from an AP.
+#define AT_WIFI_AP_CFG "AT+CWSAP"                   // [ ] Query/Set the configuration of an ESP SoftAP.
+#define AT_WIFI_AP_LIST_STATIONS "AT+CWLIF"         // [ ] Obtain IP address of the station that connects to an ESP SoftAP.
+#define AT_WIFI_AP_KICK_STATION "AT+CWQIF"          // [ ] Disconnect stations from an ESP SoftAP.
 #define AT_WIFI_DHCP "AT+CWDHCP"                    // [ ] Enable/disable DHCP.
 #define AT_WIFI_DHCP_LEASES "AT+CWDHCPS"            // [ ] Query/Set the IP addresses allocated by an ESP SoftAP DHCP server.
+#define AT_WIFI_STATION_PROTOCOL "AT+CWSTAPROTO"    // [ ] Query/Set the 802.11 b/g/n Protocol Standard of Station Mode.
+#define AT_WIFI_AP_PROTOCOL "AT+CWAPPROTO"          // [ ] Query/Set the 802.11 b/g/n Protocol Standard of SoftAP Mode.
 #define AT_WIFI_AUTOCONNECT "AT+CWAUTOCONN"         // [ ] Connect to an AP automatically when powered on.
+#define AT_WIFI_STATION_HOSTNAME "AT+CWHOSTNAME"    // [ ] Query/Set the host name of an ESP station.
+#define AT_WIFI_COUNTRY "AT+CWCOUNTRY"              // [ ] Query/Set the Wi-Fi Country Code.
 #define AT_WIFI_STATION_MAC "AT+CIPSTAMAC"          // [ ] Query/Set the MAC address of an ESP station.
 #define AT_WIFI_AP_MAC "AT+CIPAPMAC"                // [ ] Query/Set the MAC address of an ESP SoftAP.
 #define AT_WIFI_STATION_IP "AT+CIPSTA"              // [ ] Query/Set the IP address of an ESP station.
 #define AT_WIFI_AP_IP "AT+CIPAP"                    // [ ] Query/Set the IP address of an ESP SoftAP.
-#define AT_WIFI_WPS "AT+WPS"                        // [ ] Enable the WPS function.
 #define AT_WIFI_MDNS "AT+MDNS"                      // [ ] Configure the mDNS function.
-#define AT_WIFI_STATION_HOSTNAME "AT+CWHOSTNAME"    // [ ] Query/Set the host name of an ESP station.
-#define AT_WIFI_COUNTRY "AT+CWCOUNTRY"              // [ ] Query/Set the Wi-Fi Country Code.
+#define AT_WIFI_WPS "AT+WPS"                        // [ ] Enable the WPS function.
+#define AT_WIFI_START_SMARTCONFIG "AT+CWSTARTSMART" // [ ] Start SmartConfig.
+#define AT_WIFI_STOP_SMARTCONFIG "AT+CWSTARTSMART"  // [ ] Stop SmartConfig.
 
 // IP
 #define AT_IP_V6 "AT+CIPV6"                                 // [ ] Enable/disable the network of Internet Protocol Version 6 (IPv6).
@@ -78,7 +96,6 @@
 #define AT_IP_SERVER_TIMEOUT "AT+CIPSTO"                    // [ ] Query/Set the local TCP Server Timeout.
 #define AT_IP_SNTP_CFG "AT+CIPSNTPCFG"                      // [ ] Query/Set the time zone and SNTP server.
 #define AT_IP_SNTP "AT+CIPSNTPTIME"                         // [ ] Query the SNTP time.
-#define AT_IP_UPGRADE "AT+CIUPDATE"                         // [ ] Upgrade the firmware through Wi-Fi.
 #define AT_IP_SSL_LIST_CLIENTS "AT+CIPSSLCCONF"             // [ ] Query/Set SSL clients.
 #define AT_IP_SSL_CCN "AT+CIPSSLCCN"                        // [ ] Query/Set the Common Name of the SSL client.
 #define AT_IP_SSL_SNI "AT+CIPSSLCSNI"                       // [ ] Query/Set SSL client Server Name Indication (SNI).
@@ -92,17 +109,115 @@
 #define AT_IP_PING "AT+PING"                                // [ ] Ping the remote host.
 #define AT_IP_DNS "AT+CIPDNS"                               // [ ] Query/Set DNS server information.
 
-// HTTP
-#define AT_HTTP_CLIENT "AT+HTTPCLIENT"  // [ ] Send HTTP Client Request.
-#define AT_HTTP_SIZE "AT+HTTPGETSIZE"   // [ ] Get HTTP Resource Size.
-#define AT_HTTP_POST "AT+HTTPCPOST"     // [ ] Post HTTP data of specified length.
+// MQTT
+#define AT_MQTT_USER_CFG "AT+MQTTUSERCFG"               // [ ] Set MQTT User Configuration.
+#define AT_MQTT_CLIENT_ID "AT+MQTTCLIENTID"             // [ ] Set MQTT Client ID.
+#define AT_MQTT_USERNAME "AT+MQTTUSERNAME"              // [ ] Set MQTT Username.
+#define AT_MQTT_PASSWORD "AT+MQTTPASSWORD"              // [ ] Set MQTT Password.
+#define AT_MQTT_CLIENT_ID_LONG "AT+MQTTLONGCLIENTID"    // [ ] Set MQTT Client ID (Long).
+#define AT_MQTT_USERNAME_LONG "AT+MQTTLONGUSERNAME"     // [ ] Set MQTT Username (Long).
+#define AT_MQTT_PASSWORD_LONG "AT+MQTTLONGPASSWORD"     // [ ] Set MQTT Password (Long).
+#define AT_MQTT_CONNECTION_CFG "AT+MQTTCONNCFG"         // [ ] Set Configuration of MQTT Connection.
+#define AT_MQTT_CONNECT "AT+MQTTCONN"                   // [ ] Connect to MQTT Brokers.
+#define AT_MQTT_PUBLISH "AT+MQTTPUB"                    // [ ] Publish MQTT Messages in String.
+#define AT_MQTT_PUBLISH_RAW "AT+MQTTPUBRAW"             // [ ] Publish MQTT Messages in Binary.
+#define AT_MQTT_SUBSCRIBE "AT+MQTTSUB"                  // [ ] Subscribe to MQTT Topics.
+#define AT_MQTT_UNSUBSCRIBE "AT+MQTTUNSUB"              // [ ] Unsubscribe from MQTT Topics.
+#define AT_MQTT_CLOSE "AT+MQTTCLEAN"                    // [ ] Close MQTT Connections.
 
+// Host UART settings
+struct esp01_uart_settings {
+    uint baud_rate;
+    uint data_bits;
+    uint stop_bits;
+    uart_parity_t parity;
+    bool cts;
+    bool rts;
+} typedef esp01_uart_settings_t;
+
+// ESP01 instance struct
 struct esp01_inst {
     uart_inst_t *uart_inst;
-    uint baud_rate;
     uint tx_pin;
     uint rx_pin;
+    esp01_uart_settings_t uart_settings;
 } typedef esp01_inst_t;
+
+// Version struct
+struct esp01_version {
+    char *at;
+    char *sdk;
+    char *bin;
+} typedef esp01_version_t;
+
+// Sleep mode
+enum esp01_sleep_mode {
+    ESP01_SLEEP_DISABLED = 0,
+    ESP01_SLEEP_MODEM_DTIM = 1,
+    ESP01_SLEEP_MODEM_INTERVAL = 3,
+    ESP01_SLEEP_LIGHT = 2,
+} typedef esp01_sleep_mode_t;
+
+// Wifi mode
+enum esp01_wifi_mode {
+    ESP01_WIFI_DISABLED = 0,
+    ESP01_WIFI_STATION = 1,
+    ESP01_WIFI_AP = 2,
+    ESP01_WIFI_AP_AND_STATION = 3
+} typedef esp01_wifi_mode_t;
+
+// Wifi state
+enum esp01_wifi_state {
+    ESP01_WIFI_CLOSED = 0,
+    ESP01_WIFI_DISCONNECTED = 4,
+    ESP01_WIFI_CONNECTING = 3,
+    ESP01_WIFI_CONNECTED_WITHOUT_IP = 1,
+    ESP01_WIFI_CONNECTED_WITH_IP = 2,
+} typedef esp01_wifi_state_t;
+
+// Wifi properties (state and SSID)
+struct esp01_wifi_properties {
+    esp01_wifi_state_t state;
+    char ssid[ESP01_SSID_LENGTH + 1];
+} typedef esp01_wifi_properties_t;
+
+// Wifi connection error
+enum esp01_wifi_error {
+    ESP01_WIFI_ERROR_TIMEOUT = 1,
+    ESP01_WIFI_ERROR_WRONG_PWD = 2,
+    ESP01_WIFI_ERROR_UNKNOWN_AP = 3,
+    ESP01_WIFI_ERROR_FAILED = 4,
+} typedef esp01_wifi_error_t;
+
+// Wifi scan mode
+enum esp01_scan_mode {
+    ESP01_SCAN_UNDEFINED = ESP01_UNDEFINED,
+    ESP01_SCAN_FAST = 0,
+    ESP01_SCAN_ALL_CHANNEL = 1
+} typedef esp01_scan_mode_t;
+
+// Wifi PMF
+enum esp01_pmf_mode {
+    ESP01_PMF_UNDEFINED = ESP01_UNDEFINED,
+    ESP01_PMF_DISABLED = 0,
+    ESP01_PMF_CAPABLE = 1,
+    ESP01_PMF_REQUIRED = 3,
+} typedef esp01_pmf_mode_t;
+
+// Wifi connection properties struct
+struct esp01_connection_properties {
+    char ssid[ESP01_SSID_LENGTH + 1];
+    char password[ESP01_PWD_LENGTH + 1];
+    char mac[ESP01_MAC_LENGTH + 1];
+    int channel;
+    int rssi;
+    int pci_auth;
+    int reconnection_interval;
+    int listen_interval;
+    esp01_scan_mode_t scan_mode;
+    int jap_timeout;
+    esp01_pmf_mode_t pmf;
+} typedef esp01_connection_properties_t;
 
 /*!
  * Initialize a communication with ESP01 device.
@@ -131,33 +246,20 @@ void esp01_deinit(esp01_inst_t *inst);
 void esp01_reinit(esp01_inst_t *inst);
 
 /*!
+ * Get host UART configuration.
+ *
+ * @param inst Pointer to the communication instance
+ * @return UART host settings
+ */
+esp01_uart_settings_t esp01_get_host_uart(esp01_inst_t *inst);
+
+/*!
  * Set the host UART configuration.
  *
  * @param inst Pointer to the communication instance
- * @param data_bits Number of bits of data. 5..8
- * @param stop_bits Number of stop bits 1..2
- * @param parity Parity option
- * @param cts If true enable flow control of TX by clear-to-send input
- * @param rts If true enable assertion of request-to-send output by RX flow control
+ * @param uart_set UART host settings
  */
-void esp01_set_host_uart(esp01_inst_t *inst, uint data_bits, uint stop_bits, uart_parity_t parity, bool cts, bool rts);
-
-struct esp01_uart_settings {
-    uint baud_rate;
-    uint data_bits;
-    uint stop_bits;
-    uart_parity_t parity;
-    bool cts;
-    bool rts;
-} typedef esp01_uart_settings_t;
-
-/*!
- * Set the host UART configuration from device settings.
- *
- * @param inst Pointer to the communication instance
- * @param uart_set UART device settings
- */
-void esp01_set_host_uart_from_device(esp01_inst_t *inst, esp01_uart_settings_t uart_set);
+void esp01_set_host_uart(esp01_inst_t *inst, esp01_uart_settings_t uart_set);
 
 /*!
  * Send a command to the ESP01 device.
@@ -203,26 +305,14 @@ bool esp01_test(esp01_inst_t *inst);
  */
 bool esp01_reset(esp01_inst_t *inst);
 
-struct esp01_version {
-    char *at;
-    char *sdk;
-    char *bin;
-} typedef esp01_version_t;
-
 /*!
  * Get device firmware version.
  *
  * @param inst Pointer to the communication instance
+ * @param ver Pointer to the variable used to store the result
  * @return True if the command was successfully executed, false otherwise
  */
 bool esp01_get_version(esp01_inst_t *inst, esp01_version_t *ver);
-
-enum esp01_sleep_mode {
-    DISABLED = 0,
-    MODEM_DTIM = 1,
-    MODEM_INTERVAL = 3,
-    LIGHT = 2,
-} typedef esp01_sleep_mode_t;
 
 /*!
  * Get device sleep mode.
@@ -296,5 +386,51 @@ bool esp01_get_store_mode(esp01_inst_t *inst, bool *mode);
  * @return True if the command was successfully executed, false otherwise
  */
 bool esp01_set_store_mode(esp01_inst_t *inst, bool mode);
+
+/*!
+ * Get wifi mode.
+ *
+ * @param inst Pointer to the communication instance
+ * @param mode Pointer to the variable used to store the result
+ * @return True if the command was successfully executed, false otherwise
+ */
+bool esp01_get_wifi_mode(esp01_inst_t *inst, esp01_wifi_mode_t *mode);
+
+/*!
+ * Set wifi mode.
+ *
+ * @param inst Pointer to the communication instance
+ * @param mode New wifi mode
+ * @return True if the command was successfully executed, false otherwise
+ */
+bool esp01_set_wifi_mode(esp01_inst_t *inst, esp01_wifi_mode_t mode);
+
+/*!
+ * Get wifi state.
+ *
+ * @param inst Pointer to the communication instance
+ * @param state Pointer to the variable used to store the result
+ * @return True if the command was successfully executed, false otherwise
+ */
+bool esp01_get_wifi_state(esp01_inst_t *inst, esp01_wifi_properties_t *state);
+
+/*!
+ * Get wifi connection.
+ *
+ * @param inst Pointer to the communication instance
+ * @param properties Pointer to the variable used to store the result
+ * @return True if the command was successfully executed, false otherwise
+ */
+bool esp01_get_wifi_connection(esp01_inst_t *inst, esp01_connection_properties_t *properties);
+
+/*!
+ * Connect to an AP.
+ *
+ * @param inst Pointer to the communication instance
+ * @param properties Connection properties
+ * @param error_code Pointer to the variable used to store the error code
+ * @return True if the command was successfully executed, false otherwise
+ */
+bool esp01_wifi_connect(esp01_inst_t *inst, esp01_connection_properties_t properties, esp01_wifi_error_t *error_code);
 
 #endif
